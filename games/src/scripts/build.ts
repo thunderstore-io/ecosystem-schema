a5a23faa635c6175110d6564385ceab89e04fc4a
Mythic <mythic@thunderstore.io>
2023-01-30 10:57:54 +0200

    Add 4 new communities
    Add communities for:
    - Ancient Dungeon VR
    - Brotato
    - Atrio: The Dark Wild
    - Ultimate Chicken Horse

--- games/src/scripts/build.ts
+++ games/src/scripts/build.ts
@@ -22,7 +22,7 @@ for (const game of existingDefinitions) {
 }
 
 const result = {
-  schemaVersion: "0.0.2",
+  schemaVersion: "0.0.3",
   games: Object.fromEntries(games),
   communities: Object.fromEntries(communities),
 };
