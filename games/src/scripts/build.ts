856ee9fda2289f6af16e400b27bac96dff9bdee9
Mythic <mythic@thunderstore.io>
2023-05-16 10:30:05 +0300

    Bump schema version

--- games/src/scripts/build.ts
+++ games/src/scripts/build.ts
@@ -22,7 +22,7 @@ for (const game of existingDefinitions) {
 }
 
 const result = {
-  schemaVersion: "0.0.9",
+  schemaVersion: "0.0.10",
   games: Object.fromEntries(games),
   communities: Object.fromEntries(communities),
 };
